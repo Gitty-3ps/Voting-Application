4e1d1c91b815b28e2154025b7d93909d932659ae
gitty-3ps <deronhutchofficial77@gmail.com>
2025-05-01 12:51:55 -0500

    feat(fetching all polls): connect backend to front end api response

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PollComponent } from './poll/poll.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
